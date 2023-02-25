3df5524026246024ec25fac0d3514b184bc9c279
jyojun <parkcode98@gmail.com>
2023-02-25 15:58:34 +0900

    add: clock hand function

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -2,12 +2,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import timerSlice from "../../store/timer";
 import ClockFace from "../ClockFace/ClockFace";
+import { ClockWrapper, ClockHand } from "./Clock.styled";
+import { useRef } from "react";
 
 export default function Clock() {
-  const dispatch = useDispatch();
+  const clockRef = useRef(null);
   const hours = useSelector((state) => state.timer.hours);
   const minutes = useSelector((state) => state.timer.minutes);
   const seconds = useSelector((state) => state.timer.seconds);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     setInterval(() => {
@@ -17,10 +21,25 @@ export default function Clock() {
     }, 1000);
   }, []);
 
+  const secondDegree = (seconds * 6) % 360;
+  const minuteDegree = ((minutes * 6) % 360) + secondDegree / 60;
+  const hourDegree = ((hours * 6) % 360) + minuteDegree / 60;
+
   return (
-    <div>
-      {hours} {minutes} {seconds}
+    <ClockWrapper ref={clockRef}>
+      <ClockHand
+        className="hour_hand"
+        style={{ transform: `rotate(${hourDegree}deg)` }}
+      />
+      <ClockHand
+        className="minute_hand"
+        style={{ transform: `rotate(${minuteDegree}deg)` }}
+      />
+      <ClockHand
+        className="second_hand"
+        style={{ transform: `rotate(${secondDegree}deg)` }}
+      />
       <ClockFace />
-    </div>
+    </ClockWrapper>
   );
 }
---
+++ src/components/Clock/Clock.styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const ClockWrapper = styled.div`
+  position: relative;
+  max-width: 780px;
+  width: 100%;
+  margin: 50px auto;
+`;
+
+const ClockHand = styled.div`
+  position: absolute;
+  background: black;
+
+  left: calc(50% - 3px);
+  bottom: calc(50% - 2px);
+  border-radius: 5px;
+
+  transform-origin: bottom;
+  &.hour_hand {
+    height: 30%;
+    width: 10px;
+  }
+
+  &.minute_hand {
+    height: 40%;
+    width: 7px;
+    background: blue;
+    left: calc(50% - 1px);
+  }
+
+  &.second_hand {
+    height: 45%;
+    width: 3px;
+    background: red;
+    left: calc(50% + 1px);
+  }
+`;
+export { ClockWrapper, ClockHand };
